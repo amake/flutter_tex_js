a7c99f48970ef6fac7dd4d9745066783604ffb2c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-07-03 00:16:50 +0900

    Implement Dart interface

--- lib/flutter_tex_js.dart
+++ lib/flutter_tex_js.dart
@@ -1,13 +1,44 @@
 import 'dart:async';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class FlutterTexJs {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_tex_js');
+  static const MethodChannel _channel = MethodChannel('flutter_tex_js');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<Uint8List> render(String text) async =>
+      _channel.invokeMethod<Uint8List>('render', {'text': text});
+}
+
+class TexImage extends StatelessWidget {
+  const TexImage(this.math, {Key key})
+      : assert(math != null),
+        super(key: key);
+
+  final String math;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Uint8List>(
+      future: FlutterTexJs.render(math),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Image.memory(
+            snapshot.data,
+            scale: MediaQuery.of(context).devicePixelRatio,
+          );
+        } else if (snapshot.hasError) {
+          return Column(
+            children: [
+              const Icon(Icons.error),
+              Text(snapshot.error.toString()),
+            ],
+          );
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
