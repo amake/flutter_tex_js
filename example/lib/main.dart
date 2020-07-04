d27095817c184a7587e0e6b86fd8638d4a14e447
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-07-04 21:25:26 +0900

    Set dark theme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,6 +30,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter TeX JS Example'),
