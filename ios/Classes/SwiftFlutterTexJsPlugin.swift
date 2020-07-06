686a3979c787943c80415eb1ca2f7ad383185cd6
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-07-07 08:42:22 +0900

    Try to fix concurrency issues

--- ios/Classes/SwiftFlutterTexJsPlugin.swift
+++ ios/Classes/SwiftFlutterTexJsPlugin.swift
@@ -11,9 +11,9 @@ public class SwiftFlutterTexJsPlugin: NSObject, FlutterPlugin {
     @available(iOS 11.0, *)
     lazy var renderer = TexRenderer()
 
-    let queue = DispatchQueue(label: "TexJsRenderQueue", qos: .userInteractive, attributes: .concurrent)
+    let queue = DispatchQueue(label: "TexJsRenderQueue", qos: .userInteractive)
     let semaphore = DispatchSemaphore(value: 1)
-    var jobs: [String:DispatchWorkItem] = [:]
+    let jobManager = ConcurrentDictionary<DispatchWorkItem>()
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
@@ -64,53 +64,110 @@ public class SwiftFlutterTexJsPlugin: NSObject, FlutterPlugin {
 
         let cleanup = { [weak self] in
             job = nil
-            self?.jobs[requestId] = nil
             self?.semaphore.signal()
-            debugPrint("Remaining jobs: \(self?.jobs.count ?? -1)")
+            debugPrint("Remaining jobs: \(self?.jobManager.count ?? -1)")
         }
 
-        let isCancelled = { () -> Bool in
-            if job?.isCancelled ?? true {
+        let isCancelled = { [weak self] () -> Bool in
+            let queuedJob = self?.jobManager.get(requestId)
+            debugPrint("Queued job for \(requestId): \(queuedJob)")
+            let cancelled = queuedJob == nil || job! !== queuedJob!
+            if cancelled {
                 debugPrint("Job \(requestId) canceled! Exiting")
                 result(FlutterError(code: "JobCancelled", message: "The job was cancelled", details: "Request ID: \(requestId)"))
                 cleanup()
-                return true
-            } else {
-                return false
             }
+            return cancelled
         }
 
         job = DispatchWorkItem { [weak self] in
+            debugPrint("Job \(requestId) waiting; thread=\(Thread.current)")
             self?.semaphore.wait()
             guard !isCancelled() else { return }
             // WebView init has to be done on UI thread
             DispatchQueue.main.async {
+                debugPrint("Now on main thread; job=\(requestId)")
                 guard !isCancelled() else { return }
                 self?.renderer.whenReady { renderer in
+                    debugPrint("Now ready; job=\(requestId)")
                     guard !isCancelled() else { return }
                     let start = NSDate().timeIntervalSinceReferenceDate * 1000
+                    debugPrint("Going to render; job=\(requestId)")
                     renderer.render(text, displayMode: displayMode, color: color, maxWidth: maxWidth) { data, error in
+                        debugPrint("Now back from render; job=\(requestId)")
+                        guard !isCancelled() else { return }
                         let end = NSDate().timeIntervalSinceReferenceDate * 1000
-                        debugPrint("Rendering \(text) took \(Int(end - queued)) ms (\(Int(end - start)) rendering; \(Int(start - queued)) queued)")
+                        debugPrint("Rendering \(text) (\(requestId)) took \(Int(end - queued)) ms (\(Int(end - start)) rendering; \(Int(start - queued)) queued)")
                         if let data = data {
                             result(FlutterStandardTypedData(bytes: data))
                         } else {
                             result(FlutterError(code: "RenderError", message: "An error occurred during rendering", details: "\(error!)"))
                         }
                         debugPrint("Job \(requestId) complete")
+                        self?.jobManager.mapItem(id: requestId) { prev in
+                            if prev == nil || prev! === job! {
+                                return nil
+                            } else {
+                                return prev
+                            }
+                        }
                         cleanup()
                     }
                 }
             }
         }
 
-        if let existingJob = jobs[requestId] {
-            debugPrint("Canceling existing job \(requestId)")
-            existingJob.cancel()
-        }
+//        if let existingJob = jobs[requestId] {
+//            debugPrint("Canceling existing job \(requestId)")
+//            existingJob.cancel()
+//        }
 
         debugPrint("Queueing job \(requestId)")
-        jobs[requestId] = job
+        if jobManager.put(id: requestId, item: job) != nil {
+            debugPrint("Replaced existing job \(requestId)")
+        }
         queue.async(execute: job!)
+    }
+
+class ConcurrentDictionary<T> {
+    var data: [String:T] = [:]
+    let queue = DispatchQueue(label: "ConcurrentDictionaryQueue")
+
+    private var readData: [String:T] {
+        var data: [String:T]!
+        queue.sync {
+            data = self.data
+        }
+        return data
+    }
+
+    @discardableResult
+    func put(id: String, item: T?) -> T? {
+        mapItem(id: id) { prev in
+            item
+        }
+    }
+
+    @discardableResult
+    func mapItem(id: String, block: @escaping (T?) -> T?) -> T? {
+        var previous: T?
+        queue.sync(flags: .barrier) {
+            previous = self.data[id]
+            self.data[id] = block(previous)
+        }
+        return previous
+    }
+
+    @discardableResult
+    func clear(id: String) -> T? {
+        return put(id: id, item: nil)
+    }
+
+    func get(_ id: String) -> T? {
+        readData[id]
+    }
+
+    var count: Int {
+        readData.count
     }
 }
