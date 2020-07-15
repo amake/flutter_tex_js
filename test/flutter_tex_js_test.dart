7d29cef4cc87691df3055fcf4fc87533eb3b4fd0
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-07-15 21:22:45 +0900

    Implement minimal tests

--- test/flutter_tex_js_test.dart
+++ test/flutter_tex_js_test.dart
@@ -1,15 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_tex_js/flutter_tex_js.dart';
 
+// 1x1 transparent pixel generated with ImageMagick:
+//   convert -size 1x1 xc:transparent pixel.png
+// and then compressed with ImageOptim
+const pixel = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3a'
+    'AAAAAXRSTlMAQObYZgAAAApJREFUeAFjZAAAAAQAAhq+CAMAAAAASUVORK5CYII=';
+
 void main() {
-  const MethodChannel channel = MethodChannel('flutter_tex_js');
+  const channel = MethodChannel('flutter_tex_js');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+    channel.setMockMethodCallHandler((methodCall) async {
+      switch (methodCall.method) {
+        case 'render':
+          return base64.decode(pixel);
+        case 'cancel':
+          return null;
+        default:
+          assert(false,
+              'Unknown channel method called in test: ${methodCall.method}');
+      }
     });
   });
 
@@ -17,7 +34,24 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterTexJs.platformVersion, '42');
+  test('render', () async {
+    expect(
+        await FlutterTexJs.render(
+          '',
+          requestId: '',
+          color: Colors.black,
+          maxWidth: double.infinity,
+          displayMode: true,
+        ),
+        base64.decode(pixel));
+  });
+
+  testWidgets('create widget', (tester) async {
+    await tester.pumpWidget(
+      const Directionality(
+        textDirection: TextDirection.ltr,
+        child: TexImage('x^2'),
+      ),
+    );
   });
 }
