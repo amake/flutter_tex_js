e58066db8664b610d212646a8e630dee93f52a7b
Aaron Madlon-Kay <aaron@madlon-kay.com>
2026-08-11 15:04:52 +0900

    Fix tag CSS class

--- flutter_tex_js_android/android/src/main/kotlin/com/madlonkay/flutter_tex_js/TexRenderer.kt
+++ flutter_tex_js_android/android/src/main/kotlin/com/madlonkay/flutter_tex_js/TexRenderer.kt
@@ -26,7 +26,7 @@ private const val html = """
         <style type="text/css">
          body { background: transparent; margin: 0; }
          .katex-display { margin: 0; }
-         .katex-html > .tag { position: unset !important; padding-left: 2em; }
+         .katex-html > .katex-tag { position: unset !important; padding-left: 2em; }
          #math { float: left;  padding-top: 1px; padding-bottom: 1px; }
         </style>
     </head>
--- flutter_tex_js_ios/ios/flutter_tex_js_ios/Sources/flutter_tex_js_ios/TexRenderer.swift
+++ flutter_tex_js_ios/ios/flutter_tex_js_ios/Sources/flutter_tex_js_ios/TexRenderer.swift
@@ -47,7 +47,7 @@ fileprivate let html = """
         <style type="text/css">
          body { background: transparent; margin: 0; }
          .katex-display { margin: 0; padding: 1px 0; }
-         .katex-html > .tag { position: unset !important; padding-left: 2em; }
+         .katex-html > .katex-tag { position: unset !important; padding-left: 2em; }
          #math { float: left; padding-top: 1px; padding-bottom: 1px; }
         </style>
     </head>
